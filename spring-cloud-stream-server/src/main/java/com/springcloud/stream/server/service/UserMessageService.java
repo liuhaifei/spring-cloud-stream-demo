638536c6157f9f29da36667eac7a97d51e472e0f
刘海飞 <15901995342@163.com>
2019-01-03 15:16:28 +0800

    spring-cloud-stream-demo

--- spring-cloud-stream-server/src/main/java/com/springcloud/stream/server/service/UserMessageService.java
+++ spring-cloud-stream-server/src/main/java/com/springcloud/stream/server/service/UserMessageService.java
@@ -8,10 +8,12 @@ import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.SubscribableChannel;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 
 @Service
@@ -40,7 +42,11 @@ public class UserMessageService {
     public void init() {
         SubscribableChannel subscribableChannel = userMessage.input();
         subscribableChannel.subscribe(message -> {
-            System.out.println("Subscribe by SubscribableChannel:"+message);
+            try {
+                System.out.println("Subscribe by SubscribableChannel:"+new String((byte[])message.getPayload(),"UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
         });
     }
 
